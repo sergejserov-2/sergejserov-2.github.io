b227667922a597e6fb8dd25a699a14de8fd865fa
sergejserov-2 <139491645+sergejserov-2@users.noreply.github.com>
2026-04-17 23:13:03 +0300

    Update Streetview.js

--- Streetview.js
+++ Streetview.js
@@ -1,126 +1,122 @@
 export class Streetview {
-    constructor(map) {
-        this.map = map;
-        this.distributionExample = { weighted: 0, uniform: 1 };
+    constructor(mapAdapter) {
+        this.map = mapAdapter;
+
+        this.distributionExample = {
+            weighted: 0,
+            uniform: 1
+        };
+
         this.distribution = this.distributionExample.weighted;
     }
 
-    async randomValidLocation(endZoom = 14) {
-        let tile = await this.randomValidTile(endZoom);
-        let canvas = document.createElement("canvas");
-        let context = canvas.getContext("2d");
-        let img = tile.img;
-        canvas.width = img.width;
-        canvas.height = img.height;
-        context.drawImage(img, 0, 0);
-        let data = context.getImageData(0, 0, img.width, img.height).data;
-        let bluePixelCount = 0;
-        for (let i = 0; i < data.length; i += 4) {
-            if (data[i + 2] > 0) bluePixelCount++;
-        }
-        let randomPixel = Math.floor(Math.random() * bluePixelCount);
-        for (let i = 0; i < data.length; i += 4) {
-            if (data[i + 2] > 0 && --randomPixel === 0) {
-                let x = (i / 4) % img.width;
-                let y = Math.floor((i / 4) / img.width);
-                return this.tilePixelToLatLon(tile.x, tile.y, tile.zoom, x, y);
-            }
-        }
-        console.error("No blue pixel found");
-        return this.randomValidLocation(endZoom);
+    async getRandomLocation(endZoom = 14) {
+        const tile = await this.randomValidTile(endZoom);
+        return this.pickRandomPixel(tile);
     }
 
     async randomValidTile(endZoom) {
         let chosenTile = { x: 0, y: 0, zoom: 0 };
         let previousTiles = [chosenTile];
         let failedTiles = [];
+
         while (chosenTile.zoom < endZoom) {
-            let subTiles = await this.getSubTiles(
+            const subTiles = await this.map.getSubTiles(
                 chosenTile.x,
                 chosenTile.y,
                 chosenTile.zoom
             );
-            let validTiles = subTiles
-                .filter(tile => tile.hasSv)
-                .filter(tile => this.tileIntersectsMap(tile.x, tile.y, tile.zoom))
-                .filter(tile => {
-                    for (let fail of failedTiles) {
-                        if (
-                            fail.x === tile.x &&
-                            fail.y === tile.y &&
-                            fail.zoom === tile.zoom
-                        ) {
-                            return false;
-                        }
-                    }
-                    return true;
-                });
+
+            const validTiles = subTiles
+                .filter(t => t.hasSv)
+                .filter(t => this.tileIntersectsMap(t));
+
             if (validTiles.length === 0) {
                 failedTiles.push(chosenTile);
-                if (previousTiles.length > 0)
-                    chosenTile = previousTiles.splice(-2)[0];
-                else
-                    chosenTile = { x: 0, y: 0, zoom: 0 };
-                console.log(
-                    "Позиция не найдена, поиск..." +
-                        chosenTile.zoom,
-                    chosenTile
-                );
+
+                chosenTile =
+                    previousTiles.length > 0
+                        ? previousTiles.splice(-2)[0]
+                        : { x: 0, y: 0, zoom: 0 };
             } else {
                 chosenTile = this.pickRandomSubTile(validTiles);
                 previousTiles.push(chosenTile);
             }
         }
+
         return chosenTile;
     }
 
-    pickRandomSubTile(tiles) {
-        if (this.distribution === this.distributionExample.uniform) {
-            return tiles[Math.floor(Math.random() * tiles.length)];
-        }
-        let totalCoverage = tiles
-            .map(tile => tile.coverage)
-            .reduce((a, b) => a + b, 0);
-        let random = Math.random() * totalCoverage;
-        for (let tile of tiles) {
-            random -= tile.coverage;
-            if (random <= 0) return tile;
-        }
-        console.error("Не найти плитку");
-    }
+    tileIntersectsMap(tile) {
+        const bounds = [
+            this.tilePixelToLatLon(tile.x, tile.y, tile.zoom, 0, 0),
+            this.tilePixelToLatLon(tile.x, tile.y, tile.zoom, 256, 256),
+            this.tilePixelToLatLon(tile.x, tile.y, tile.zoom, 0, 256),
+            this.tilePixelToLatLon(tile.x, tile.y, tile.zoom, 256, 0),
+        ];
 
-    tileIntersectsMap(tileX, tileY, zoom) {
-        let bounds = [];
-        bounds.push(
-            this.tilePixelToLatLon(tileX, tileY, zoom, 0, 0)
-        );
-        bounds.push(
-            this.tilePixelToLatLon(tileX, tileY, zoom, 256, 256)
-        );
-        bounds.push(
-            this.tilePixelToLatLon(tileX, tileY, zoom, 0, 256)
-        );
-        bounds.push(
-            this.tilePixelToLatLon(tileX, tileY, zoom, 256, 0)
-        );
-        for (let bound of bounds) {
-            if (this.map.isInMap(bound.lat, bound.lng)) {
+        for (const b of bounds) {
+            if (this.map.isInMap(b.lat, b.lng)) {
                 return true;
             }
         }
-        let mapsBounds = new google.maps.LatLngBounds(
-            new google.maps.LatLng(bounds[2].lat, bounds[2].lng),
-            new google.maps.LatLng(bounds[3].lat, bounds[3].lng)
-        );
 
-        let intersect = false;
-        this.map.polygon.getPaths().forEach(path => {
-            path.forEach(point => {
-                if (mapsBounds.contains(point)) {
-                    intersect = true;
-                }
+        if (this.map.polygon) {
+            const rect = new google.maps.LatLngBounds(
+                new google.maps.LatLng(bounds[2].lat, bounds[2].lng),
+                new google.maps.LatLng(bounds[3].lat, bounds[3].lng)
+            );
+
+            let intersect = false;
+
+            this.map.polygon.getPaths().forEach(path => {
+                path.forEach(p => {
+                    if (rect.contains(p)) intersect = true;
+                });
             });
-        });
-        return intersect;
+
+            return intersect;
+        }
+
+        return false;
+    }
+
+    pickRandomPixel(tile) {
+        const canvas = document.createElement("canvas");
+        const ctx = canvas.getContext("2d");
+
+        const img = tile.img;
+
+        canvas.width = img.width;
+        canvas.height = img.height;
+
+        ctx.drawImage(img, 0, 0);
+
+        const data = ctx.getImageData(0, 0, img.width, img.height).data;
+
+        let bluePixelCount = 0;
+
+        for (let i = 0; i < data.length; i += 4) {
+            if (data[i + 2] > 0) bluePixelCount++;
+        }
+
+        let randomPixel = Math.floor(Math.random() * bluePixelCount);
+
+        for (let i = 0; i < data.length; i += 4) {
+            if (data[i + 2] > 0 && --randomPixel === 0) {
+                const x = (i / 4) % img.width;
+                const y = Math.floor((i / 4) / img.width);
+
+                return this.tilePixelToLatLon(
+                    tile.x,
+                    tile.y,
+                    tile.zoom,
+                    x,
+                    y
+                );
+            }
+        }
+
+        throw new Error("No valid pixel found");
     }
 }
