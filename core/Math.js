99d843aff694e38a60ab49dba6bd33e72a671217
sergejserov-2 <139491645+sergejserov-2@users.noreply.github.com>
2026-04-18 17:01:25 +0300

    Update Math.js

--- core/Math.js
+++ core/Math.js
@@ -1,5 +1,18 @@
 export class Math {
     
+    distance(from, to) {
+        return google.maps.geometry.spherical.computeDistanceBetween(
+            new google.maps.LatLng(from.lat, from.lng),
+            new google.maps.LatLng(to.lat, to.lng)
+        );
+    }
+
+    formatDistance(meters) {
+        if (meters < 1000) return `${Math.floor(meters)} м`;
+        if (meters < 20000) return `${Math.floor(meters / 100) / 10} км`;
+        return `${Math.floor(meters / 1000)} км`;
+    }
+
     calculateScore(distance) {
         const max = 5000;
         return Math.max(0, Math.round(max - distance));
