26dc46a0762d2ec95097d804187e9b99f291b140
sergejserov-2 <139491645+sergejserov-2@users.noreply.github.com>
2026-04-18 17:31:28 +0300

    Update Area.js

--- area/Area.js
+++ area/Area.js
@@ -1,66 +1,24 @@
-import { AreaRules } from "./AreaRules.js";
-
-export class Area {
-
-    constructor() {
-        this.areas = this.createAreas();
+export const area = [
+    {
+        id: "area_1",
+        name: "Central Zone",
+        polygonPoints: [
+            { lat: 54.9, lng: 23.9 },
+            { lat: 54.91, lng: 23.95 },
+            { lat: 54.89, lng: 23.96 },
+            { lat: 54.88, lng: 23.92 }
+        ],
+        minimumDistanceForPoints: 5000
+    },
+    {
+        id: "area_2",
+        name: "Industrial Zone",
+        polygonPoints: [
+            { lat: 54.92, lng: 23.85 },
+            { lat: 54.93, lng: 23.9 },
+            { lat: 54.91, lng: 23.91 },
+            { lat: 54.90, lng: 23.86 }
+        ],
+        minimumDistanceForPoints: 8000
     }
-
-    createAreas() {
-        return [
-            {
-                id: "area_1",
-                name: "Central Zone",
-                polygonPoints: [
-                    { lat: 54.9, lng: 23.9 },
-                    { lat: 54.91, lng: 23.95 },
-                    { lat: 54.89, lng: 23.96 },
-                    { lat: 54.88, lng: 23.92 }
-                ],
-                minimumDistanceForPoints: 5000,
-                rules: new AreaRules(
-                    [
-                        { lat: 54.9, lng: 23.9 },
-                        { lat: 54.91, lng: 23.95 },
-                        { lat: 54.89, lng: 23.96 },
-                        { lat: 54.88, lng: 23.92 }
-                    ],
-                    5000,
-                    "Central Zone"
-                )
-            },
-
-            {
-                id: "area_2",
-                name: "Industrial Zone",
-                polygonPoints: [
-                    { lat: 54.92, lng: 23.85 },
-                    { lat: 54.93, lng: 23.9 },
-                    { lat: 54.91, lng: 23.91 },
-                    { lat: 54.90, lng: 23.86 }
-                ],
-                minimumDistanceForPoints: 8000,
-                rules: new AreaRules(
-                    [
-                        { lat: 54.92, lng: 23.85 },
-                        { lat: 54.93, lng: 23.9 },
-                        { lat: 54.91, lng: 23.91 },
-                        { lat: 54.90, lng: 23.86 }
-                    ],
-                    8000,
-                    "Industrial Zone"
-                )
-            }
-        ];
-    }
-
-    // вернуть все Area
-    getAll() {
-        return this.areas;
-    }
-
-    // получить по id
-    getById(id) {
-        return this.areas.find(area => area.id === id);
-    }
-}
+];
