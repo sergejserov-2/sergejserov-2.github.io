45cbb54b666b17678b2f20d897258d5762e2ecda
sergejserov-2 <139491645+sergejserov-2@users.noreply.github.com>
2026-04-19 02:56:22 +0300

    Update AreaRegistry.js

--- area/AreaRegistry.js
+++ area/AreaRegistry.js
@@ -6,8 +6,10 @@ import LatinAmerica from "./regions/LatinAmerica.js";
 import Oceania from "./regions/Oceania.js";
 import MiddleEast from "./regions/MiddleEast.js";
 import SovietUnion from "./regions/SovietUnion.js";
+import Moscow from "./test/Moscow.js";
 
 export const AreaRegistry = {
+    moscow: Moscow, //test
     europe: Europe,
     asia: Asia,
     africa: Africa,
