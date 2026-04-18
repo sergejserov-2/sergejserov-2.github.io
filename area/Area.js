52b9a510f7a6103d2025d02b2670460a141fad46
sergejserov-2 <139491645+sergejserov-2@users.noreply.github.com>
2026-04-19 00:05:19 +0300

    Update Area.js

--- area/Area.js
+++ area/Area.js
@@ -4,3 +4,23 @@ export class Area {
         this.polygon = polygon;
     }
 }
+
+import Europe from "./regions/Europe.js";
+import Asia from "./regions/Asia.js";
+import Africa from "./regions/Africa.js";
+import NorthAmerica from "./regions/NorthAmerica.js";
+import LatinAmerica from "./regions/LatinAmerica.js";
+import Oceania from "./regions/Oceania.js";
+import MiddleEast from "./regions/MiddleEast.js";
+import SovietUnion from "./regions/SovietUnion.js";
+
+export const Areas = {
+    europe: Europe,
+    asia: Asia,
+    africa: Africa,
+    north_america: NorthAmerica,
+    latin_america: LatinAmerica,
+    oceania: Oceania,
+    middleEast: MiddleEast,
+    sovietUnion: SovietUnion,
+};
