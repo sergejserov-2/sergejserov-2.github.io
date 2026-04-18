9f4eddc130f5a7b6d671f2208fbd2fa45d810221
sergejserov-2 <139491645+sergejserov-2@users.noreply.github.com>
2026-04-18 21:45:25 +0300

    Update MapAdapter.js

--- infrastructure/MapAdapter.js
+++ infrastructure/MapAdapter.js
@@ -2,31 +2,21 @@ export class MapAdapter {
     constructor(google) {
         this.google = google;
     }
-
     hasStreetView(lat, lng) {
         const sv = new this.google.maps.StreetViewService();
 
         return new Promise((resolve) => {
             sv.getPanorama(
-                { location: { lat, lng }, radius: 50 },
+                {
+                    location: { lat, lng },
+                    radius: 50
+                },
                 (data, status) => {
-                    resolve(status === this.google.maps.StreetViewStatus.OK);
+                    resolve(
+                        status === this.google.maps.StreetViewStatus.OK
+                    );
                 }
             );
         });
-    }
-
-    containsPoint(point, polygon) {
-        return this.google.maps.geometry.poly.containsLocation(
-            new this.google.maps.LatLng(point.lat, point.lng),
-            polygon
-        );
-    }
-
-    distance(a, b) {
-        return this.google.maps.geometry.spherical.computeDistanceBetween(
-            new this.google.maps.LatLng(a.lat, a.lng),
-            new this.google.maps.LatLng(b.lat, b.lng)
-        );
     }
 }
