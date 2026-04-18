82ddf6864bdc99e009599e84189a02cceeb0f704
sergejserov-2 <139491645+sergejserov-2@users.noreply.github.com>
2026-04-18 17:00:01 +0300

    Update Math.js

--- core/Math.js
+++ core/Math.js
@@ -1,3 +1,22 @@
-export class Math () {
-  
+export class Math {
+    
+    calculateScore(distance) {
+        const max = 5000;
+        return Math.max(0, Math.round(max - distance));
+    }
+
+    calculateResult({ guess, actual, time }) {
+        if (!guess || !actual) {
+            return { score: 0, distance: 0 };
+        }
+
+        const distance = this.distance(guess, actual);
+        const score = this.calculateScore(distance);
+
+        return {
+            distance,
+            formatted: this.formatDistance(distance),
+            score
+        };
+    }
 }
