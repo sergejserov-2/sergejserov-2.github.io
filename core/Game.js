fd9bd07d704b3bb0cd229b5793d299d837268ef6
sergejserov-2 <139491645+sergejserov-2@users.noreply.github.com>
2026-04-19 21:46:22 +0300

    Update Game.js

--- core/Game.js
+++ core/Game.js
@@ -1,8 +1,5 @@
-import { Emitter } from "./Emitter.js";
-
-export class Game extends Emitter {
+export class Game {
  constructor({ gameState, scoring }) {
-  super();
   this.state = gameState;
   this.scoring = scoring;
   this.isLocked = false;
@@ -11,28 +8,17 @@ export class Game extends Emitter {
  startGame() {
   this.state.start();
   this.state.status = "active";
-  this.fire("gameStarted");
  }
 
  startRound(location) {
   this.state.startRound(location);
   this.isLocked = false;
-
-  this.fire("roundStarted", {
-   round: this.state.currentRound.index,
-   actual: location
-  });
  }
 
  setGuess(playerId, point) {
   if (!this.state.currentRound || this.isLocked) return;
 
   this.state.setGuess(playerId, point);
-
-  this.fire("guessUpdated", {
-   playerId,
-   guess: point
-  });
  }
 
  finishGuess(playerId = "p1") {
@@ -41,30 +27,19 @@ export class Game extends Emitter {
   const round = this.state.currentRound;
   if (!round) return;
 
-  const guess = this.state.getGuess(playerId);
-  if (!guess) return;
+  const guessObj = this.state.getGuess(playerId);
+  if (!guessObj) return;
 
   const result = this.scoring.calculateResult({
-   guess: guess.guess,
+   guess: guessObj.guess,
    actual: round.actualLocation
   });
 
   this.state.applyGuessResult(playerId, result);
-
   this.isLocked = true;
-
-  this.fire("guessFinished", {
-   round: round.index,
-   result,
-   guess: guess.guess,
-   actual: round.actualLocation
-  });
  }
 
  endGame() {
   this.state.endGame();
-  this.fire("gameEnded", {
-   rounds: this.state.rounds
-  });
  }
 }
