fcba204cc7ba317692bc44219a4ca3f4d49be462
sergejserov-2 <139491645+sergejserov-2@users.noreply.github.com>
2026-04-20 01:42:49 +0300

    Update AreaRegistry.js

--- domain/AreaRegistry.js
+++ domain/AreaRegistry.js
@@ -1,31 +1,37 @@
-import { europe } from "./area/regions/Europe.js";
-import { asia } from "./area/regions/Asia.js";
-import { africa } from "./area/regions/Africa.js";
-import { northAmerica } from "./area/regions/NorthAmerica.js";
-import { latinAmerica } from "./area/regions/LatinAmerica.js";
-import { oceania } from "./area/regions/Oceania.js";
-import { middleEast } from "./area/regions/MiddleEast.js";
-import { sovietUnion } from "./area/regions/SovietUnion.js";
-import { moscow } from "./area/test/Moscow.js";
+import { europe } from "../data/areas/Europe.js";
+import { asia } from "../data/areas/Asia.js";
+import { africa } from "../data/areas/Africa.js";
+import { northAmerica } from "../data/areas/NorthAmerica.js";
+import { latinAmerica } from "../data/areas/LatinAmerica.js";
+import { oceania } from "../data/areas/Oceania.js";
+import { middleEast } from "../data/areas/MiddleEast.js";
+import { sovietUnion } from "../data/areas/SovietUnion.js";
+import { moscow } from "../data/areas/Moscow.js";
 
 const rawAreas = {
-    europe, asia, africa, northAmerica, latinAmerica, oceania, middleEast, sovietUnion,
-    moscow
+ europe, asia, africa, northAmerica, latinAmerica, oceania, middleEast, sovietUnion, moscow
 };
 
 function toRuntime(area) {
  return {
   name: area.name,
   minDistance: area.minDistance,
-  polygon: area.polygon.map(([lat, lng]) => ({ lat, lng }))
+  polygon: area.polygon.map(([lat, lng]) => ({
+   lat,
+   lng
+  }))
  };
 }
 
 export const AreaRegistry = {
  get(name) {
   const area = rawAreas[name];
-  if (!area) { throw new Error(`Area "${name}" not found`); }
+  if (!area) {
+   throw new Error(Area "${name}" not found);
+  }
   return toRuntime(area);
  },
- list() { return Object.keys(rawAreas); }
+ list() {
+  return Object.keys(rawAreas);
+ }
 };
