0571b67337518f76a6a4300a4f31e61dd8bac9bd
sergejserov-2 <139491645+sergejserov-2@users.noreply.github.com>
2026-04-19 18:54:53 +0300

    Update Bridge.js

--- core/Bridge.js
+++ core/Bridge.js
@@ -12,25 +12,33 @@ export class Bridge {
             this.ui.static.showGame();
         });
 
-        this.game.on("roundStarted", (round) => {
+        this.game.on("roundStarted", ({ round, actual }) => {
+            const state = this.game.state;
+
             this.ui.static.updateHUD(
-                this.vm.buildHUD(round)
+                this.vm.buildHUD(state, { index: round })
             );
 
+            this.ui.streetview?.setLocation(actual);
             this.ui.map?.reset?.();
         });
 
-        this.game.on("guessFinished", (round) => {
+        this.game.on("guessFinished", ({ round }) => {
+            const state = this.game.state;
+
             this.ui.static.updateHUD(
-                this.vm.buildHUD(round)
+                this.vm.buildHUD(state, { index: round })
             );
         });
 
-        this.game.on("roundCommitted", (round) => {
+        this.game.on("roundCommitted", () => {
+            const state = this.game.state;
+            const round = state.rounds.at(-1);
+
             this.ui.map?.lock?.();
 
             this.ui.static.showRoundResult(
-                this.vm.buildRoundVM(round)
+                this.vm.buildRoundVM(state, round)
             );
         });
 
