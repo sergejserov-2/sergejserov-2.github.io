5147eaf4e174ed747504b391c92a16ed00ab9d60
sergejserov-2 <139491645+sergejserov-2@users.noreply.github.com>
2026-04-19 18:06:45 +0300

    Update StreetviewUI.js

--- ui/StreetviewUI.js
+++ ui/StreetviewUI.js
@@ -1,1 +1,23 @@
+export class StreetViewUI {
+    constructor({ adapter, element }) {
+        this.adapter = adapter;
+        this.element = element;
+        this.panorama = null;
+    }
 
+    init() {
+        this.panorama = this.adapter.createPanorama(this.element);
+    }
+
+    setLocation([lat, lng]) {
+        if (!this.panorama) return;
+
+        this.adapter.setPanoramaPosition(this.panorama, lat, lng);
+    }
+
+    reset() {
+        if (!this.panorama) return;
+
+        this.adapter.resetPanorama?.(this.panorama);
+    }
+}
