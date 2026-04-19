65e4726701d0b46adf79e1739ccaf98709f315a5
sergejserov-2 <139491645+sergejserov-2@users.noreply.github.com>
2026-04-19 18:43:11 +0300

    Update GameFlow.js

--- core/GameFlow.js
+++ core/GameFlow.js
@@ -3,31 +3,16 @@ export class GameFlow {
         this.game = game;
         this.generator = generator;
         this.area = area;
-
-        this.currentRound = 0;
     }
 
     async startGame() {
-        this.currentRound = 0;
         this.game.startGame();
-
         await this.nextRound();
     }
 
     async nextRound() {
         const location = await this.generator.generate(this.area);
-
         this.game.startRound(location);
-
-        this.currentRound++;
-    }
-
-    onGuessFinish() {
-        this.game.commitRound();
-    }
-
-    async continue() {
-        await this.nextRound();
     }
 
     endGame() {
