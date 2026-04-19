d18cdb41a35b9596b8d71d8a0dcb2ddd5d914b3f
sergejserov-2 <139491645+sergejserov-2@users.noreply.github.com>
2026-04-19 18:12:27 +0300

    Update MapAdapter.js

--- adapters/MapAdapter.js
+++ adapters/MapAdapter.js
@@ -1,22 +1,62 @@
 export class MapAdapter {
-    constructor(google) {
-        this.google = google;
+    constructor() {
+        this.svService = new google.maps.StreetViewService();
     }
-    hasStreetView(lat, lng) {
-        const sv = new this.google.maps.StreetViewService();
 
-        return new Promise((resolve) => {
-            sv.getPanorama(
-                {
-                    location: { lat, lng },
-                    radius: 50
-                },
+    createMap(element, { center = [0,0], zoom = 2 } = {}) {
+        return new google.maps.Map(element, {
+            center: { lat: center[0], lng: center[1] },
+            zoom,
+            disableDefaultUI: true
+        });
+    }
+
+    createPanorama(element) {
+        return new google.maps.StreetViewPanorama(element, {
+            disableDefaultUI: true
+        });
+    }
+
+    setPanoramaPosition(panorama, lat, lng) {
+        panorama.setPosition({ lat, lng });
+    }
+
+    async hasStreetView(lat, lng) {
+        return new Promise(resolve => {
+            this.svService.getPanorama(
+                { location: { lat, lng }, radius: 50000 },
                 (data, status) => {
-                    resolve(
-                        status === this.google.maps.StreetViewStatus.OK
-                    );
+                    resolve(status === google.maps.StreetViewStatus.OK);
                 }
             );
         });
+    }
+
+    createMarker(map, [lat, lng]) {
+        return new google.maps.Marker({
+            position: { lat, lng },
+            map
+        });
+    }
+
+    moveMarker(marker, [lat, lng]) {
+        marker.setPosition({ lat, lng });
+    }
+
+    removeMarker(marker) {
+        marker.setMap(null);
+    }
+
+    fitToMarkers(map, markers) {
+        const bounds = new google.maps.LatLngBounds();
+        markers.forEach(m => {
+            const pos = m.getPosition();
+            if (pos) bounds.extend(pos);
+        });
+        map.fitBounds(bounds);
+    }
+
+    resetPanorama(panorama) {
+        panorama.setPov({ heading: 0, pitch: 0 });
     }
 }
