a4b422c451e457b27fe892dcc9ced37a61c60bcf
sergejserov-2 <139491645+sergejserov-2@users.noreply.github.com>
2026-04-19 19:59:11 +0300

    Update Bridge.js

--- core/Bridge.js
+++ core/Bridge.js
@@ -1,62 +1,69 @@
 export class Bridge {
-  constructor({ game, mapUI, streetViewUI, staticUI, viewModelBuilder }) {
-    this.game = game;
-    this.mapUI = mapUI;
-    this.streetViewUI = streetViewUI;
-    this.staticUI = staticUI;
-    this.vm = viewModelBuilder;
-    
-    this.bind();
-  }
-  
-  bind() {
-    
-    /* GAME START */
-    this.game.on("gameStarted", () => {
-      this.staticUI.hideLoading();
-    });
-    
-    /* ROUND START */
-    this.game.on("roundStarted", ({ round, roundCount }) => {
-      this.staticUI.updateHUD(
-      this.vm.buildHUD(this.game.state, this.game.state.currentRound)
-    );
-    
-    this.streetViewUI.setLocation(this.game.state.currentRound.actualLocation);
-    this.mapUI.resetGuess();
-    });
-    
-    /* GUESS UPDATE */
-    this.game.on("guessUpdated", ({ playerId, guess }) => {
-      this.mapUI.placeGuessMarker(guess);
-    });
-    
-    /* GUESS FINISHED */
-    this.game.on("guessFinished", ({ result }) => {
-    
-      const vm = this.vm.buildRoundVM(
-      this.game.state,
-      this.game.state.currentRound
-      );
-      
-      this.staticUI.showRoundResult(vm);
-      this.mapUI.renderOverview({
-        guess: result.guess,
-        actual: result.actual
-      });
-    });
-    
-    /* ROUND COMMIT */
-    this.game.on("roundCommitted", () => {
-      this.game.startRound(this.game.state.area);
-    });
-    
-    /* GAME END */
-    this.game.on("gameEnded", () => {
-    
-      const vm = this.vm.buildGameVM(this.game.state);
-      
-      this.staticUI.showGameResult(vm);
-    });
-  }
+ constructor({ game, mapUI, streetViewUI, staticUI, viewModelBuilder }) {
+  this.game = game;
+  this.mapUI = mapUI;
+  this.streetViewUI = streetViewUI;
+  this.staticUI = staticUI;
+  this.vm = viewModelBuilder;
+
+  this.bind();
+ }
+
+ bind() {
+
+  /* GAME START */
+  this.game.on("gameStarted", () => {
+   this.staticUI.hideLoading();
+  });
+
+  /* ROUND START */
+  this.game.on("roundStarted", () => {
+
+   this.staticUI.updateHUD(
+    this.vm.buildHUD(this.game.state, this.game.state.currentRound)
+   );
+
+   const location = this.game.state.currentRound.actualLocation;
+
+   this.streetViewUI.onReady(() => {
+    this.streetViewUI.setLocation(location);
+   });
+
+   this.mapUI.resetGuess();
+  });
+
+  /* GUESS UPDATE */
+  this.game.on("guessUpdated", ({ guess }) => {
+   this.mapUI.placeGuessMarker(guess);
+  });
+
+  /* GUESS FINISHED */
+  this.game.on("guessFinished", ({ result }) => {
+
+   const vm = this.vm.buildRoundVM(
+    this.game.state,
+    this.game.state.currentRound
+   );
+
+   this.staticUI.showRoundResult(vm);
+
+   this.mapUI.renderOverview({
+    guess: result.guess,
+    actual: result.actual
+   });
+  });
+
+  /* ROUND COMMIT */
+  this.game.on("roundCommitted", () => {
+   this.game.startRound(this.game.state.area);
+  });
+
+  /* GAME END */
+  this.game.on("gameEnded", () => {
+
+   const vm = this.vm.buildGameVM(this.game.state);
+
+   this.staticUI.showGameResult(vm);
+  });
+ }
 }
