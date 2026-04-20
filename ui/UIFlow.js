3008d95e871e8c25f3006d5508cbf1171ed03e58
sergejserov-2 <139491645+sergejserov-2@users.noreply.github.com>
2026-04-20 15:58:14 +0300

    Update UIFlow.js

--- ui/UIFlow.js
+++ ui/UIFlow.js
@@ -9,17 +9,18 @@ export class UIFlow {
  }
 
  bind() {
-  this.gameFlow.on("gameStarted", vm => {
+
+  this.gameFlow.on("gameStarted", (state) => {
+   const vm = this.uiBuilder.formatGameVM(state);
+
    this.screenManager.show("round");
-   this.staticUI.updateHUD(
-    this.uiBuilder.formatHUD(vm)
-   );
+   this.staticUI.updateHUD(vm);
   });
 
-  this.gameFlow.on("stateUpdated", vm => {
-   this.staticUI.updateHUD(
-    this.uiBuilder.formatHUD(vm)
-   );
+  this.gameFlow.on("stateUpdated", (state) => {
+   const vm = this.uiBuilder.formatGameVM(state);
+
+   this.staticUI.updateHUD(vm);
   });
 
   this.gameFlow.on("inputLocked", () => {
@@ -30,20 +31,18 @@ export class UIFlow {
    this.staticUI.unlockInput?.();
   });
 
-  this.gameFlow.on("roundEnded", vm => {
-   this.screenManager.show("roundResult");
+  this.gameFlow.on("roundEnded", (state) => {
+   const vm = this.uiBuilder.formatRoundVM(state);
 
-   this.staticUI.showRoundResult(
-    this.uiBuilder.formatRoundResult(vm)
-   );
+   this.screenManager.show("roundResult");
+   this.staticUI.showRoundResult(vm);
   });
 
-  this.gameFlow.on("gameEnded", vm => {
-   this.screenManager.show("gameResult");
+  this.gameFlow.on("gameEnded", (state) => {
+   const vm = this.uiBuilder.formatGameResultVM(state);
 
-   this.staticUI.showGameResult(
-    this.uiBuilder.formatGameResult(vm)
-   );
+   this.screenManager.show("gameResult");
+   this.staticUI.showGameResult(vm);
   });
  }
 }
