49637e406aec69d27b99b4df71186770790713ed
sergejserov-2 <139491645+sergejserov-2@users.noreply.github.com>
2026-04-20 03:58:01 +0300

    Update StaticUI.js

--- ui/components/StaticUI.js
+++ ui/components/StaticUI.js
@@ -2,72 +2,29 @@ export class StaticUI {
  constructor({ element }) {
   this.element = element;
 
-  this.roundEl = element?.querySelector(".round");
-  this.scoreEl = element?.querySelector(".total-score");
-  this.timeEl = element?.querySelector(".time-left");
-  this.movesEl = element?.querySelector(".moves-left");
-
-  this.loading = element?.querySelector(".loading-screen");
-  this.result = element?.querySelector(".guess-overview");
-
-  this.progress = element?.querySelector(".score-progress");
-  this.text = element?.querySelectorAll(".score-text p");
-  this.endButtons = element?.querySelector(".game-end-buttons");
+  this.roundEl = element.querySelector(".round b");
+  this.totalEl = element.querySelector(".total-score b");
+  this.progressEl = element.querySelector(".progress");
  }
 
  setScreen(screen) {
-  if (!this.loading || !this.result) return;
-
-  this.loading.style.display = "none";
-  this.result.classList.remove("active");
-
-  if (screen === "loading") {
-   this.loading.style.display = "flex";
-  }
-
-  if (screen === "result") {
-   this.result.classList.add("active");
-  }
+  document.body.dataset.screen = screen;
  }
 
- updateHUD(vm) {
-  if (this.roundEl) this.roundEl.innerText = vm.roundText;
-  if (this.scoreEl) this.scoreEl.innerText = vm.scoreText;
-  if (this.timeEl) this.timeEl.innerText = vm.timeText;
-  if (this.movesEl) this.movesEl.innerText = vm.movesText;
+ updateHUD(hud) {
+  this.roundEl.textContent = hud.roundText;
+  this.totalEl.textContent = hud.totalText;
+
+  if (this.progressEl) {
+   this.progressEl.style.width = `${hud.progress * 100}%`;
+  }
  }
 
  showRoundResult(vm) {
-  this.setScreen("result");
-
-  if (this.progress) {
-   this.progress.style.width = vm.progress + "%";
-  }
-
-  if (this.text?.length >= 2) {
-   this.text[0].innerText = vm.distanceText;
-   this.text[1].innerText = vm.totalScoreText;
-  }
-
-  if (this.endButtons) {
-   this.endButtons.style.display = "none";
-  }
+  console.log("ROUND RESULT", vm);
  }
 
  showGameResult(vm) {
-  this.setScreen("result");
-
-  if (this.progress) {
-   this.progress.style.width = vm.progress + "%";
-  }
-
-  if (this.text?.length >= 2) {
-   this.text[0].innerText = vm.lastResultText;
-   this.text[1].innerText = vm.finalScoreText;
-  }
-
-  if (this.endButtons) {
-   this.endButtons.style.display = "flex";
-  }
+  console.log("GAME RESULT", vm);
  }
 }
