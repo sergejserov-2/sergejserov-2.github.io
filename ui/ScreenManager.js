61569c5f99813f7d0d753a1fc3b027d2048620e2
sergejserov-2 <139491645+sergejserov-2@users.noreply.github.com>
2026-04-20 14:52:48 +0300

    Update ScreenManager.js

--- ui/ScreenManager.js
+++ ui/ScreenManager.js
@@ -4,7 +4,7 @@ export class ScreenManager {
 
   this.views = {
    loading: root.querySelector(".loading-screen"),
-   round: document.querySelector(".game-scene"),
+   round: root.querySelector(".game-scene"),
    roundResult: root.querySelector(".round-result"),
    gameResult: root.querySelector(".game-result")
   };
@@ -13,19 +13,14 @@ export class ScreenManager {
  show(name) {
   Object.values(this.views).forEach(v => {
    if (!v) return;
+   v.classList.remove("active");
    v.style.display = "none";
   });
 
   const target = this.views[name];
   if (!target) return;
 
-  // game-scene — базовый слой
-  if (name === "round") {
-   target.style.display = "block";
-   return;
-  }
-
-  // overlay экраны
   target.style.display = "flex";
+  target.classList.add("active");
  }
 }
