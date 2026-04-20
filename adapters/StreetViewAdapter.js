79e2e664868b2b4ee0e0ce57a222224ec6bb390a
sergejserov-2 <139491645+sergejserov-2@users.noreply.github.com>
2026-04-21 00:52:37 +0300

    Update StreetViewAdapter.js

--- adapters/StreetViewAdapter.js
+++ adapters/StreetViewAdapter.js
@@ -6,18 +6,19 @@ export class StreetViewAdapter {
  createStreetView(element, { lat = 0, lng = 0 } = {}) {
   if (!element) throw new Error("StreetView container missing");
 
-return new google.maps.StreetViewPanorama(element, {
- position: { lat, lng },
- pov: { heading: 0, pitch: 0 },
-
- addressControl: false,
- showRoadLabels: false,
- fullscreenControl: false,
- zoomControl: true,
- disableDefaultUI: false,
- compassControl: true
-});
-
+  return new google.maps.StreetViewPanorama(element, {
+   position: { lat, lng },
+   pov: { heading: 0, pitch: 0 },
+  
+   addressControl: false,
+   showRoadLabels: false,
+   fullscreenControl: false,
+   zoomControl: true,
+   disableDefaultUI: false,
+   compassControl: true
+  });
+ }
+ 
  async getStreetViewMeta({ lat, lng }) {
   return new Promise(resolve => {
    this.svService.getPanorama(
