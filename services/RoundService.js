eea15512db61f45fdb0eb1e6e5fdf6684f77fce6
sergejserov-2 <139491645+sergejserov-2@users.noreply.github.com>
2026-04-21 02:37:15 +0300

    Create RoundService.js

---
+++ services/RoundService.js
@@ -0,0 +1,23 @@
+export class RoundService {
+  constructor() {
+    this.limit = 0;
+    this.current = 0;
+  }
+
+  start(limit) {
+    this.limit = limit;
+    this.current = 0;
+  }
+
+  next() {
+    this.current++;
+  }
+
+  getCurrent() {
+    return this.current;
+  }
+
+  isFinished() {
+    return this.limit !== -1 && this.current >= this.limit;
+  }
+}
