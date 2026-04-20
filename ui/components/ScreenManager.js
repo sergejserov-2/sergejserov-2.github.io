306d1b53008053f922f175fce70a269dc4219203
sergejserov-2 <139491645+sergejserov-2@users.noreply.github.com>
2026-04-20 04:44:00 +0300

    Update ScreenManager.js

--- ui/components/ScreenManager.js
+++ ui/components/ScreenManager.js
@@ -1,32 +1,28 @@
 export class ScreenManager {
  constructor({ screensElement }) {
-  if (!screensElement) {
-   throw new Error("ScreenManager: screensElement is required");
-  }
-
   this.screensElement = screensElement;
 
   this.loading = screensElement.querySelector(".loading-screen");
-  this.result = screensElement.querySelector(".guess-overview");
+  this.roundResult = screensElement.querySelector(".round-result");
+  this.gameResult = screensElement.querySelector(".game-result");
  }
 
  setScreen(screen) {
-  this.screensElement.dataset.screen = screen;
-
-  if (this.loading) this.loading.style.display = "none";
-  if (this.result) this.result.style.display = "none";
+  [this.loading, this.roundResult, this.gameResult].forEach(el => {
+   if (el) el.style.display = "none";
+  });
 
   switch (screen) {
    case "loading":
-    if (this.loading) this.loading.style.display = "block";
+    if (this.loading) this.loading.style.display = "flex";
     break;
 
    case "result":
-    if (this.result) this.result.style.display = "block";
+    if (this.roundResult) this.roundResult.style.display = "flex";
     break;
 
-   case "round":
-   default:
+   case "gameover":
+    if (this.gameResult) this.gameResult.style.display = "flex";
     break;
   }
  }
