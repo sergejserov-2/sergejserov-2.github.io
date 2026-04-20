12f4bd5cf165e5bc6ab6695c43088563e40dbdae
sergejserov-2 <139491645+sergejserov-2@users.noreply.github.com>
2026-04-20 15:51:47 +0300

    Update and rename tweaks.js to Tweaks.js

---
+++ ui/Tweaks.js
@@ -0,0 +1,52 @@
+const observer = new MutationObserver((mutations) => {
+ mutations.forEach(mutation => {
+  mutation.addedNodes.forEach(node => {
+   if (
+    node.nodeType === 1 &&
+    node.tagName !== "CANVAS" &&
+    node.tagName !== "IFRAME" &&
+    node.children.length === 0 &&
+    node.textContent?.includes("For development purposes only")
+   ) {
+    node.style.display = "none";
+   }
+  });
+ });
+});
+
+const observer2 = new MutationObserver(() => {
+ const el = document.querySelector(
+  ".mapsImagerySceneScene__root.widget-scene"
+ );
+
+ if (el) {
+  el.style.filter = "invert(1)";
+ }
+});
+
+export class Tweaks {
+ constructor() {
+  this.started = false;
+ }
+
+ apply() {
+  if (this.started) return;
+  this.started = true;
+
+  observer.observe(document.body, {
+   childList: true,
+   subtree: true
+  });
+
+  observer2.observe(document.body, {
+   childList: true,
+   subtree: true
+  });
+ }
+
+ destroy() {
+  observer.disconnect();
+  observer2.disconnect();
+  this.started = false;
+ }
+}
