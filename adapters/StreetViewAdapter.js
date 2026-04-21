0d24f6321c871a59240ae9217c4e84ed021563ed
sergejserov-2 <139491645+sergejserov-2@users.noreply.github.com>
2026-04-22 01:02:00 +0300

    Update StreetViewAdapter.js

--- adapters/StreetViewAdapter.js
+++ adapters/StreetViewAdapter.js
@@ -39,6 +39,15 @@ export class StreetViewAdapter {
   }, 1000);
  }
 
+setControls(panorama, enabled) {
+ if (!panorama) return;
+
+ panorama.setOptions({
+  panControl: enabled,
+  linksControl: enabled
+ });
+}
+ 
  async getStreetViewMeta({ lat, lng }) {
   return new Promise(resolve => {
    this.svService.getPanorama(
