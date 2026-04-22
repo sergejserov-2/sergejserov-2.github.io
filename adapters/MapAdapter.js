2200249150d5622415f86ac0369297b4d92f219d
sergejserov-2 <139491645+sergejserov-2@users.noreply.github.com>
2026-04-23 00:11:57 +0300

    Update MapAdapter.js

--- adapters/MapAdapter.js
+++ adapters/MapAdapter.js
@@ -12,7 +12,7 @@ export class MapAdapter {
 
         const map = new maplibregl.Map({
             container: element,
-            style: `https://api.maptiler.com/maps/019db4a6-96e9-70d5-a214-f01c8c0ea283/style.json?key=${key}`,
+            style: `https://api.maptiler.com/maps/019db4b1-7dea-76e9-b311-977e10dcd80c/style.json?key=${key}`,
             center: this.toLngLat(center),
             zoom,
             attributionControl: false
