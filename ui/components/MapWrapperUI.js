bbe28ee5234a540d222258004c3f6216132a69b5
sergejserov-2 <139491645+sergejserov-2@users.noreply.github.com>
2026-04-22 15:51:07 +0300

    Update MapWrapperUI.js

--- ui/components/MapWrapperUI.js
+++ ui/components/MapWrapperUI.js
@@ -1,84 +1,73 @@
 export class MapWrapperUI {
-    constructor({ adapter, element, uiBuilder }) {
-        this.adapter = adapter;
-        this.uiBuilder = uiBuilder;
-        this.element = element;
+ constructor({ adapter, element, uiBuilder }) {
+  this.adapter = adapter;
+  this.uiBuilder = uiBuilder;
+  this.element = element;
 
-        this.map = null;
-        this.guessMarker = null;
-        this.isLocked = false;
+  this.map = null;
+  this.guessMarker = null;
+  this.lastGuessPoint = null;
+  this.isLocked = false;
 
-        this.onGuess = null;
-        this.lastGuessPoint = null;
+  this._resizeObserver = null;
+ }
 
-        this.area = null;
+ init() {
+  this.map = this.adapter.createMap(this.element, {
+   zoom: 2,
+   center: { lat: 20, lng: 0 }
+  });
 
-        this.polygon = null;
-        this.polygonVisible = false;
+  // 🔥 стабильный resize
+  this._resizeObserver = new ResizeObserver(() => {
+   this.adapter.resize(this.map);
+  });
 
-        this._resizeObserver = null;
-    }
+  this._resizeObserver.observe(this.element);
 
-    init() {
-        this.map = this.adapter.createMap(this.element, {
-            zoom: 2,
-            center: { lat: 20, lng: 0 }
-        });
+  this.map.on("click", (e) => {
+   if (this.isLocked) return;
 
-        // 🔥 стабильный resize всегда
-        this._resizeObserver = new ResizeObserver(() => {
-            this.adapter.resize(this.map);
-        });
+   const point = {
+    lat: e.lngLat.lat,
+    lng: e.lngLat.lng
+   };
 
-        this._resizeObserver.observe(this.element);
+   this.lastGuessPoint = point;
+   this.placeGuessMarker(point);
+  });
+ }
 
-        this.map.on("click", (e) => {
-            if (this.isLocked) return;
+ placeGuessMarker(point) {
+  this.clearGuessMarker();
 
-            const point = {
-                lat: e.lngLat.lat,
-                lng: e.lngLat.lng
-            };
+  this.guessMarker = this.adapter.createMarker(
+   this.map,
+   point,
+   {
+    color: this.uiBuilder.getPlayerColor("p1")
+   }
+  );
+ }
 
-            this.lastGuessPoint = point;
-            this.placeGuessMarker(point);
-        });
-    }
+ clearGuessMarker() {
+  if (!this.guessMarker) return;
 
-    setArea(area) {
-        this.area = area;
-    }
+  this.adapter.removeMarker(this.guessMarker);
+  this.guessMarker = null;
+ }
 
-    placeGuessMarker(point) {
-        this.clearGuessMarker();
+ reset() {
+  this.clearGuessMarker();
+  this.lastGuessPoint = null;
+  this.unlock();
+ }
 
-        this.guessMarker = this.adapter.createMarker(
-            this.map,
-            point,
-            {
-                color: this.uiBuilder.getPlayerColor("p1")
-            }
-        );
-    }
+ lock() {
+  this.isLocked = true;
+ }
 
-    clearGuessMarker() {
-        if (!this.guessMarker) return;
-
-        this.adapter.removeMarker(this.guessMarker);
-        this.guessMarker = null;
-    }
-
-    reset() {
-        this.unlock();
-        this.clearGuessMarker();
-        this.lastGuessPoint = null;
-    }
-
-    lock() {
-        this.isLocked = true;
-    }
-
-    unlock() {
-        this.isLocked = false;
-    }
+ unlock() {
+  this.isLocked = false;
+ }
 }
