e7da45beacac479a7aa3ebd84c83c6df084001cb
sergejserov-2 <139491645+sergejserov-2@users.noreply.github.com>
2026-04-23 16:05:32 +0300

    Update firebaseApp.js

--- server/firebaseApp.js
+++ server/firebaseApp.js
@@ -1,17 +1,21 @@
- import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-database.js";
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyBINTgjBBLq7pn-oi6tlxAqSgb0spkPSIs",
-    authDomain: "mini-game-by-loc.firebaseapp.com",
-    databaseURL: "https://mini-game-by-loc-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "mini-game-by-loc",
-    storageBucket: "mini-game-by-loc.firebasestorage.app",
-    messagingSenderId: "934025050812",
-    appId: "1:934025050812:web:1c2d15239575857bc32eb9"
-  };
+const firebaseConfig = {
+ apiKey: "AIzaSyBINTgjBBLq7pn-oi6tlAxx...",
+ authDomain: "mini-game-by-loc.firebaseapp.com",
+ databaseURL: "https://mini-game-by-loc-default-rtdb.europe-west1.firebasedatabase.app",
+ projectId: "mini-game-by-loc",
+ storageBucket: "mini-game-by-loc.firebasestorage.app",
+ messagingSenderId: "934025050812",
+ appId: "1:934025050812:web:1c2d15239575857bc32eb9"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+// =========================
+// INIT FIREBASE
+// =========================
+const app = initializeApp(firebaseConfig);
+
+// 🔥 ВАЖНО: создаём DB
+export const db = getDatabase(app);
+export const firebaseApp = app;
